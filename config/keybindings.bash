b42b9562581538ef58be95b3e9376171811fc290
Amr Alaa <n1amr1@gmail.com>
2017-03-05 15:56:18 +0200

    add keybindings

---
+++ config/keybindings.bash
@@ -0,0 +1,36 @@
+bind '"\C-p": history-search-backward'
+bind '"\C-n": history-search-forward'
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+bind '"\eOA": history-search-backward'
+bind '"\eOB": history-search-forward'
+bind '"\C-r": reverse-search-history'
+bind '"\C-s": forward-search-history'
+
+bind '"\C-a": beginning-of-line'
+bind '"\C-e": end-of-line'
+bind '"\C-b": backward-char'
+bind '"\C-f": forward-char'
+
+bind '"\C-h": backward-delete-char'
+bind '"\C-k": kill-line'
+bind '"\C-u": unix-line-discard'
+bind '"\C-w": unix-word-rubout'
+
+bind '"\C-_": undo'
+bind '"\C-j": accept-line'
+bind '"\C-l": clear-screen'
+bind '"\C-o": overwrite-mode'
+bind '"\C-v": quoted-insert'
+bind '"\C-x\C-e": edit-and-execute-command'
+bind '"\C-x\C-k": "\C-e\C-u"' # push-line alternative
+bind '"\C-xa": alias-expand-line'
+bind '"\C-y": yank'
+
+bind '"\C-x\C-u" "\C-e\C-ucd ..\n\C-y"'
+
+bind '"\C-gd" "\C-e\C-ugit diff\n\C-y"'
+bind '"\C-gl" "\C-e\C-ugit l\n\C-y"'
+bind '"\C-gs" "\C-e\C-ugit st\n\C-y"'
+
+bind '"\e[15~" "\C-p\C-j"'
---
+++ config/keybindings.zsh
@@ -0,0 +1,37 @@
+bindkey "^P" up-line-or-beginning-search
+bindkey "^[OA" up-line-or-beginning-search
+bindkey "^N" down-line-or-beginning-search
+bindkey "^[OB" down-line-or-beginning-search
+bindkey "^R" history-incremental-search-backward
+bindkey "^S" history-incremental-search-forward
+
+bindkey "^A" beginning-of-line
+bindkey "^E" end-of-line
+bindkey "^B" backward-char
+bindkey "^F" forward-char
+
+bindkey "^H" backward-delete-char
+bindkey "^K" kill-line
+bindkey "^U" kill-whole-line
+bindkey "^W" backward-kill-word
+
+bindkey "^J" accept-line
+bindkey "^L" clear-screen
+bindkey "^O" overwrite-mode
+bindkey "^Q" push-line
+bindkey "^V" quoted-insert
+bindkey "^X^E" edit-command-line
+bindkey "^Xa" _expand_alias
+bindkey "^Y" yank
+bindkey "^[[Z" reverse-menu-complete
+bindkey "^_" undo
+bindkey -s "^X^K" "^E^U"
+
+bindkey -s "^B" "^Qd^M"
+bindkey -s "^[u" "^Qcd ..^M"
+
+bindkey -s "^Gd" "^Qgit diff^M"
+bindkey -s "^Gl" "^Qgit l^M"
+bindkey -s "^Gs" "^Qgit st^M"
+
+bindkey -s "^[[15~" "^P^J"
