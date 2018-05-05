dc464fd4bbb8c8f0ebb5ef748ac3d7e502672359
Amr Alaa <n1amr1@gmail.com>
2018-05-06 01:53:58 +0200

    config/i3/scripts/focus-window.sh: add window class

--- config/i3/scripts/focus-window.sh
+++ config/i3/scripts/focus-window.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-selected_line="$(wmctrl -l | cut -d ' ' --complement -f '2-4' | dmenu -i -p 'Focus window:')"
+selected_line="$(wmctrl -lx | awk '{$2=$4=""; print}' | dmenu -i -p 'Focus window:')"
 selected_id="$(echo "$selected_line" | cut -d ' ' -f 1)"
 wmctrl -i -a "$selected_id"
