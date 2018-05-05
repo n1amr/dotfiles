56a5dadd083271b3036093f95e4eadf523772b49
Amr Alaa <n1amr1@gmail.com>
2018-05-05 02:31:24 +0200

    config/i3/dunstrc: change output format to 3 lines

--- config/i3/scripts/start-dunst.sh
+++ config/i3/scripts/start-dunst.sh
@@ -5,5 +5,6 @@ source ~/.dotfiles_config
 DUNST_CONFIG_FILE="$DOTFILES_HOME/config/i3/dunstrc"
 DUNST_BIN="$DOTFILES_HOME/config/i3/bin/dunst"
 
-killall dunst
+# depends on: apt install libxdg-basedir1
+killall -9 dunst
 "$DUNST_BIN" -config "$DUNST_CONFIG_FILE" &
