99f3bb35728a999bf72e50410a5a0688f393dd66
Amr Alaa <n1amr1@gmail.com>
2018-05-31 15:28:09 +0200

    config/i3/i3blocks/widgets/mediaplayer-control-widget.sh: increase responsiveness

--- config/i3/i3blocks/widgets/mediaplayer-control-widget.sh
+++ config/i3/i3blocks/widgets/mediaplayer-control-widget.sh
@@ -7,15 +7,12 @@ ICON_PREV=''
 ICON_STOP=''
 
 MEDIA_CONTROLLER="$(dirname "${BASH_SOURCE[0]}")/../../scripts/media-control.sh"
-if ! "$MEDIA_CONTROLLER" is-running > /dev/null 2>&1; then
-    exit
-fi
-
 BLOCK_INSTANCE="${BLOCK_INSTANCE:-status}"
 
 handle_next()   { "$MEDIA_CONTROLLER" next; }
 handle_prev()   { "$MEDIA_CONTROLLER" prev; }
 handle_toggle() { "$MEDIA_CONTROLLER" play-pause; }
+handle_status_click() { "$MEDIA_CONTROLLER" notification; }
 
 handle_click() {
     case "$BLOCK_INSTANCE" in
@@ -68,16 +65,23 @@ render_status() {
     echo "$short_status"
 }
 
-handle_status_click() {
-    "$MEDIA_CONTROLLER" notification
+handle_events() {
+    case "$BLOCK_BUTTON" in
+        1) handle_click ;;
+    esac
 }
 
-case "$BLOCK_BUTTON" in
-    1) handle_click ;;
-esac
+render() {
+    if ! "$MEDIA_CONTROLLER" is-running > /dev/null 2>&1; then
+        exit
+    fi
 
-case "$BLOCK_INSTANCE" in
-    status)   render_status                   ;;
-    button-*) render_button "$BLOCK_INSTANCE" ;;
-    *)        echo 'INVALID BLOCK_INSTANCE'   ;;
-esac
+    case "$BLOCK_INSTANCE" in
+        status)   render_status                   ;;
+        button-*) render_button "$BLOCK_INSTANCE" ;;
+        *)        echo 'INVALID BLOCK_INSTANCE'   ;;
+    esac
+}
+
+handle_events &
+render &
